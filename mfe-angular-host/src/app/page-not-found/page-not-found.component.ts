ea8fc0cb65eb1defe7a8696d7d50c35175f86383
pvshake <pvshakeu@gmail.com>
2025-02-01 01:02:30 -0300

    configurando primeng e tailwind. bora fazer tudo melhor....

--- mfe-angular-host/src/app/page-not-found/page-not-found.component.ts
+++ mfe-angular-host/src/app/page-not-found/page-not-found.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.scss'
+  styleUrl: './page-not-found.component.scss',
 })
-export class PageNotFoundComponent {
-
-}
+export class PageNotFoundComponent {}
